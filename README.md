2135c814f06f2ef481a6d1c7fc70aceb506c0e01
becbutelli <c.butelli@be-tse.it>
2023-11-13 02:07:01 +0100

    moved from repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# Spring Boot Application with Spring Data Rest
+
+The service is a basic example of how Spring Data Rest works and how it helps to minimize the boilerplate controller 
+layer code.
+
+It provides CRUD Rest APIs for all the created entities without creating CONTROLLERS and SERVICES.
+
+It allows to change the base Rest API path.
+
+It offers both Pagination and Sorting support and all the queries in the repo are automatically provided as Rest 
+controller.
---
+++ src/main/java/com/cloudera/springdatarest/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.cloudera.springdatarest.repository;
+
+import com.cloudera.springdatarest.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findByFirstName(@Param("firstName") String firstName);
+}
